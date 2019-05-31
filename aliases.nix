222819dc04c48a4e7dcdca7b887499fab3cb7187
Richard Marko <srk@48.io>
2019-05-31 12:15:54 +0200

    more aliases

--- aliases.nix
+++ aliases.nix
@@ -6,6 +6,10 @@ with lib;
   environment.shellAliases = {
     ll = "ls -l";
     gg = "git grep -i";
+    nrb = "nixos-rebuild build";
+    nrs = "nixos-rebuild switch";
+    vi = "vim";
+    psg = "ps axuf | grep -i";
     nixpaste = "curl -F \"text=<-\" http://nixpaste.lbr.uno";
   };
 }
